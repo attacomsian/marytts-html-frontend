ed1ca1dc9905217c728464dde44f43da5092760c
Atta-Ur-Rehman Shah <supercomsian1989@gmail.com>
2017-08-19 15:36:14 +0500

    basic layout setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdMenuModule, MdCardModule, MdIconModule, MdButtonModule, MdToolbarModule} from '@angular/material';
 
-// import hammerjs for tooltips
+import {AppComponent} from './app.component';
+
 import 'hammerjs';
 
 @NgModule({
@@ -11,9 +15,18 @@ import 'hammerjs';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdMenuModule,
+    MdIconModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
